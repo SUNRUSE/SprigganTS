6164493afd81de63c64b912620c5ad0cb21b68f1
jameswilddev <jameswilddev@outlook.com>
2017-07-15 00:41:01 +0100

    Added the configuration file.

---
+++ Source/Engine/Configuration.ts
@@ -0,0 +1,3 @@
+const ResolutionX = 428
+const ResolutionY = 240
+const Crop = true
--- Source/Engine/Entry.ts
+++ Source/Engine/Entry.ts
@@ -1,4 +1,5 @@
 /// <reference path="LoadingMessage.ts" />
 /// <reference path="Misc.ts" />
+/// <reference path="Configuration.ts" />
 /// <reference path="Timers.ts" />
 /// <reference path="Start.ts" />
