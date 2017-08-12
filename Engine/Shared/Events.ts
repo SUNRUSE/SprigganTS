77b6c3737a035b1eaf7cbaa2d817d592775e44fa
jameswilddev <jameswilddev@outlook.com>
2017-08-12 09:05:03 +0100

    Updated events to match API definition.

--- Engine/Shared/Events.ts
+++ Engine/Shared/Events.ts
@@ -1,54 +1,52 @@
-namespace Events {
-    export class Once<T extends Function> {
-        private Raised: boolean
-        private RaisedWith: any
-        private readonly Listeners: T[] = []
+class OneTimeEvent<T extends Function> {
+    private Raised: boolean
+    private RaisedWith: any
+    private readonly Listeners: T[] = []
 
-        constructor() {
-            const thisReference = this
+    constructor() {
+        const thisReference = this
 
-            this.Raise = function () {
-                if (thisReference.Raised) return
-                thisReference.Raised = true
-                thisReference.RaisedWith = arguments
-                for (const listener of thisReference.Listeners) listener.apply(thisReference, arguments)
-            } as any
-        }
-
-        readonly Listen = (listener: T) => {
-            if (Contains(this.Listeners, listener)) return
-            this.Listeners.push(listener)
-            if (this.Raised) listener.apply(this, this.RaisedWith)
-        }
-
-        readonly Unlisten = (listener: T) => {
-            if (this.Raised) return
-            Remove(this.Listeners, listener)
-        }
-
-        readonly Raise: T
+        this.Raise = function () {
+            if (thisReference.Raised) return
+            thisReference.Raised = true
+            thisReference.RaisedWith = arguments
+            for (const listener of thisReference.Listeners) listener.apply(thisReference, arguments)
+        } as any
     }
 
-    export class Recurring<T extends Function> {
-        private readonly Listeners: T[] = []
-
-        constructor() {
-            const thisReference = this
-
-            this.Raise = function () {
-                for (const listener of thisReference.Listeners.slice()) listener.apply(thisReference, arguments)
-            } as any
-        }
-
-        readonly Listen = (listener: T) => {
-            if (Contains(this.Listeners, listener)) return
-            this.Listeners.push(listener)
-        }
-
-        readonly Unlisten = (listener: T) => {
-            Remove(this.Listeners, listener)
-        }
-
-        readonly Raise: T
+    Listen(listener: T): void {
+        if (Contains(this.Listeners, listener)) return
+        this.Listeners.push(listener)
+        if (this.Raised) listener.apply(this, this.RaisedWith)
     }
+
+    Unlisten(listener: T): void {
+        if (this.Raised) return
+        Remove(this.Listeners, listener)
+    }
+
+    readonly Raise: T
+}
+
+class RecurringEvent<T extends Function> {
+    private readonly Listeners: T[] = []
+
+    constructor() {
+        const thisReference = this
+
+        this.Raise = function () {
+            for (const listener of thisReference.Listeners.slice()) listener.apply(thisReference, arguments)
+        } as any
+    }
+
+    Listen(listener: T): void {
+        if (Contains(this.Listeners, listener)) return
+        this.Listeners.push(listener)
+    }
+
+    Unlisten(listener: T): void {
+        Remove(this.Listeners, listener)
+    }
+
+    readonly Raise: T
 }
