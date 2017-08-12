42e2a11b37ccb459e74dfca6d1e57ba6bae226a2
jameswilddev <jameswilddev@outlook.com>
2017-08-12 19:02:51 +0100

    Added types for a reworked build system.

---
+++ BuildContent/Types.ts
@@ -0,0 +1,56 @@
+type Build = {
+    LastModified: { [filename: string]: number }
+
+    ImportedContent: {
+        [contentTypeFirstExtension: string]: {
+            [filename: string]: {
+                [contentName: string]: any
+            }
+        }
+    }
+
+    PackedContent: {
+        [contentTypeFirstExtension: string]: {
+            [contentName: string]: any
+        }
+    }
+}
+
+type ImportedSpriteFrame =
+    {
+        readonly Empty: false
+        readonly PngFilename: string
+        readonly OffsetLeftPixels: number
+        readonly OffsetTopPixels: number
+        readonly DurationSeconds: number
+    } | {
+        readonly Empty: true
+        readonly DurationSeconds: number
+    }
+
+type PackedSpriteFrame =
+    {
+        readonly Empty: false
+        readonly AtlasLeftPixels: number
+        readonly AtlasTopPixels: number
+        readonly WidthPixels: number
+        readonly HeightPixels: number
+        readonly OffsetLeftPixels: number
+        readonly OffsetTopPixels: number
+        readonly DurationSeconds: number
+    } | {
+        readonly Empty: true
+        readonly DurationSeconds: number
+    }
+
+type ImportedBackground = {
+    readonly PngFilename: string
+    readonly WidthPixels: number
+    readonly HeightPixels: number
+}
+
+type PackedBackground = {
+    readonly Id: number
+    readonly WidthPixels: number
+    readonly HeightPixels: number
+}
