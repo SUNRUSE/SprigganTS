469973e2eb39cb783f872dffa19db60abe5c7e68
jameswilddev <jameswilddev@outlook.com>
2017-08-13 10:28:28 +0100

    Added types for the new ContentType structure.

---
+++ BuildContent/ContentType.ts
@@ -0,0 +1,17 @@
+const ContentTypes: ContentType<any, any, any>[] = []
+
+class ContentType<Imported, Packed, PackingHeader> {
+    readonly ContentTypeImports: ContentTypeImport<Imported>[] = []
+
+    constructor(public readonly FirstExtension: string, public readonly Pack: (imported: { [contentName: string]: Imported }, then: (header: PackingHeader, packed: { [contentName: string]: Packed }) => void) => void) {
+        ContentTypes.push(this)
+    }
+}
+
+class ContentTypeImport<Imported> {
+    constructor(public readonly ContentType: ContentType<Imported, any, any>, public readonly SecondExtension: string, public readonly Import: (filename: string, then: (content: { [contentName: string]: Imported }) => void) => void) {
+        ContentType.ContentTypeImports.push(this)
+    }
+}
+
+export { ContentTypes, ContentType, ContentTypeImport }
