ea39603ab58dc7b4805c82287387fdd63b55ee7d
jameswilddev <jameswilddev@outlook.com>
2017-08-22 22:19:39 +0100

    Fixed STSRealloc macro generating uncompilable code.  However, it requires a cast to avoid a warning; need to review this macro (part of #46).

--- Engine/Native/Allocation.h
+++ Engine/Native/Allocation.h
@@ -2,7 +2,7 @@
 
 void *STSPerformMalloc(size_t size, const char *message);
 
-#define STSRealloc(type, pointer, count) STSReallocSize(sizeof(type), pointer, count, "Failed to reallocate memory on line " STS_TO_STRING(__LINE__) " of file " __FILE__)
+#define STSRealloc(type, pointer, count) STSPerformRealloc(sizeof(type) * (count), pointer, "Failed to reallocate memory on line " STS_TO_STRING(__LINE__) " of file " __FILE__)
 #define STSReallocSize(size, pointer, count) STSPerformRealloc((size) * (count), pointer, "Failed to reallocate memory on line " STS_TO_STRING(__LINE__) " of file " __FILE__)
 
 void STSPerformRealloc(size_t size, void **pointer, const char *message);
